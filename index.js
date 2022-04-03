ad5d963bc4018b941516a4a721a03be27d66b3e0
imj22 <98693440+imj22@users.noreply.github.com>
2022-04-03 20:38:34 +0200

    commit tree 5fafdfebb917f77e86a6ff2a631715c1910170cd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+const firstArr = ["frog", "secondSun", "Mr.", 20]
+
+const createEmployeeRecord = (array) => {
+   const foo = {
+        "firstName" : array[0], 
+        "familyName" : array[1], 
+        "title" : array[2] , 
+        "payPerHour" : array[3],
+        "timeInEvents" : [],
+        "timeOutEvents" : []
+    }
+    return foo;
+}
+
+console.log(createEmployeeRecord(firstArr))
+const secondArr = ["dog", "Smith", "Ms.", 100]
+
+const createEmployeeRecords = (arrOfArrs) => {
+    const newEmployeeArr = arrOfArrs.map((arr) => {
+        return createEmployeeRecord(arr);
+    })
+    // return newEmployeeArr;
+    return newEmployeeArr
+}
+
+createEmployeeRecords([...firstArr, ...secondArr])
